3e963ef23ee08e38071c3a7844cf1db9c2b04190
凤之兮原 <1577310596@qq.com>
2023-01-09 22:38:20 +0800

    feat: add first tcpserver

--- Learning/s1/tcpserver/src/main.rs
+++ Learning/s1/tcpserver/src/main.rs
@@ -1,3 +1,10 @@
+use std::net::TcpListener;
 fn main() {
-    println!("Hello, world!");
+    let listner = TcpListener::bind("127.0.0.1:3000").unwrap();
+    println!("Running on port 3000...");
+
+    for stream in listner.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
