8d51e4ccacfb818c033b55dfc6f2aa5e0e52c7a8
凤之兮原 <1577310596@qq.com>
2023-01-10 22:17:52 +0800

    chore: add tcplient and tcpserver

--- Learning/s1/tcpclient/src/main.rs
+++ Learning/s1/tcpclient/src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpStream;
+use std::io::{ Read, Write };
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+
+    let mut buffer = [0; 5];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Resposne from server: {:?}", str::from_utf8(&buffer).unwrap());
 }
--- Learning/s1/tcpserver/src/main.rs
+++ Learning/s1/tcpserver/src/main.rs
@@ -1,10 +1,15 @@
-use std::net::TcpListener;
+use std::io::{ Read, Write };
+use std::{ net::TcpListener };
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port 3000...");
 
     for stream in listner.incoming() {
-        let _stream = stream.unwrap();
+        let mut stream = stream.unwrap();
         println!("Connection established!");
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
     }
 }
