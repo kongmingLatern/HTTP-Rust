e3355b41735b6947e3deb6798b8551716a002ce1
凤之兮原 <1577310596@qq.com>
2023-01-10 22:17:59 +0800

    feat: add http

--- Learning/s1/src/main.rs
+++ Learning/s1/src/main.rs
@@ -2,4 +2,9 @@ use std::net::TcpListener;
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port 3000...");
+
+    for stream in listner.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
