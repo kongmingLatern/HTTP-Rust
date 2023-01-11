3d5f3e691489d14374c61c2fed0ede5c360a305b
凤之兮原 <1577310596@qq.com>
2023-01-11 21:07:53 +0800

    update: update httprequest.rs

--- Learning/s1/http/src/httprequest.rs
+++ Learning/s1/http/src/httprequest.rs
@@ -109,7 +109,6 @@ impl From<&str> for Version {
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
@@ -128,10 +127,13 @@ mod tests {
     fn test_http_request() {
         let s: String = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.71.1\r\nAccept: */*\r\n\r\n");
         let mut headers_expected: HashMap<String, String> = HashMap::new();
-        headers_expected.insert("HOST".into(), " localhost".into());
+        headers_expected.insert("Host".into(), " localhost".into());
         headers_expected.insert("Accept".into(), " */*".into());
         headers_expected.insert("User-Agent".into(), " curl/7.71.1".into());
         let req: HttpRequest = s.into();
         assert_eq!(Method::Get, req.method);
+        assert_eq!(Version::V1_1, req.version);
+        assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
+        assert_eq!(headers_expected, req.headers);
     }
 }
